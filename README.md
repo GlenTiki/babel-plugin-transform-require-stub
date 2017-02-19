f6fc94390cae292b4dddb2bda25758bbe5405857
Glen Keane <glenkeane.94@gmail.com>
2017-02-19 20:51:22 +0000

    v2 ready

--- README.md
+++ README.md
@@ -1,3 +1,53 @@
-## babel-plugin-transform-require-stub
+# babel-plugin-transform-require-stub
 
 Started as a fork of babel-plugin-transform-require-ignore
+
+Ever wanted to test Webpacked frontend code... without webpack?
+
+Well this lets you do just that, by stubbing out required files with specified extensions in your code, to achieve creating stubs where a webpack loader would be called.
+
+## Installation
+
+```sh
+  npm install --save-dev babel-plugin-transform-require-stub
+```
+
+## Usage
+
+Add the plugin to your .babelrc or package.json with a config like so:
+
+```json
+{
+  "plugins": [
+    [
+      "babel-plugin-transform-require-stub",
+      {
+        "extensions": [
+          ".png", ".sass", ".styl"
+        ],
+        // this is used if there is no ".ext" property on the options object
+        "defaultStub": {
+          // replaces with the require path with the path supplied here to a stub, so require("./style$SOME_EXT_DEFINED_ABOVE") becomes require("$ABSOLUTE_PATH_TO_FILE")
+          "file": "./fileToStub"
+        },
+        ".sass": {
+          // replaces the require call with just the path string, so `require("./style.sass")` becomes just `"./style.sass"`
+          "path": true
+        },
+        ".styl": {
+          // value can be any valid json (string literal, number literal, boolean literal, null literal, object literal, or array literal)
+          // the value is then inserted into the code where the require call is matched
+          // eg require('styles.styl') would become { something: "here" }
+          "value": {
+            "something": "here"
+          }
+        }
+      }
+    ]
+  ]
+}
+```
+
+## License
+
+MIT
--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
 import path from 'path'
+import _ from 'lodash'
 
-export default function () {
+export default function (obj) {
+  const t = obj.types
   function extFix (ext) {
     return ext.charAt(0) === '.' ? ext : ('.' + ext)
+  }
+
+  function toObjectExpression (obj) {
+    return Object.keys(obj).map((key) => t.objectProperty(t.identifier(key), mapProp(obj[key])))
+  }
+
+  function toArrayExpression (arr) {
+    return arr.map((item) => mapProp(item))
+  }
+
+  function mapProp (prop) {
+    if (_.isBoolean(prop)) {
+      return t.booleanLiteral(prop)
+    } else if (_.isString(prop)) {
+      return t.stringLiteral(prop)
+    } else if (_.isNumber(prop)) {
+      return t.NumericLiteral(prop)
+    } else if (_.isPlainObject(prop)) {
+      return t.objectExpression(toObjectExpression(prop))
+    } else if (_.isArray(prop)) {
+      return t.arrayExpression(toArrayExpression(prop))
+    } else if (_.isNull(prop)) {
+      return t.nullLiteral()
+    } else {
+      throw new Error(`Unknown type of prop ${prop}`)
+    }
+  }
+
+  function optionsToExpressionStub (options, value) {
+    if (options.path) {
+      return t.stringLiteral(value)
+    } else if (options.value) {
+      return mapProp(options.value)
+    } else if (options.file) {
+      const filePath = path.isAbsolute(options.file) ? options.file : path.resolve(process.cwd(), options.file)
+      return t.callExpression(t.identifier('require'), [t.stringLiteral(filePath)])
+    } else {
+      throw new Error(`Cant handle properties on options. Expects properties of path, value or file`)
+    }
   }
 
   return {
@@ -17,13 +58,15 @@ export default function () {
           const callee = nodePath.get('callee')
 
           if (callee.isIdentifier() && callee.equals('name', 'require')) {
-            console.error(`Nodepath: ${nodePath}`)
             const arg = nodePath.get('arguments')[0]
-            if (arg && arg.isStringLiteral() && extensions.indexOf(path.extname(arg.node.value)) > -1) {
-              if (nodePath.parentPath.isVariableDeclarator()) {
-                throw new Error(`${arg.node.value} should not assign to variable.`)
+            const ext = path.extname(arg.node.value)
+            if (arg && arg.isStringLiteral() && extensions.indexOf(ext) > -1) {
+              if (opts[ext]) {
+                return nodePath.replaceWith(t.expressionStatement(optionsToExpressionStub(opts[ext])))
+              } else if (opts.defaultStub) {
+                return nodePath.replaceWith(t.expressionStatement(optionsToExpressionStub(opts.defaultStub)))
               } else {
-                nodePath.remove()
+                return nodePath.replaceWith(t.expressionStatement(t.stringLiteral(arg.node.value)))
               }
             }
           }
